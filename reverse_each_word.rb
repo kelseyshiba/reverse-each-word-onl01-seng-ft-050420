ff6e2b8ebe48aebdcce7254d0b444cee2c7ac6e8
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-08 16:10:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,15 @@
+# def reverse_each_word(string)
+#   reversed = (string.split.collect do |word|
+#     word.reverse
+#   end).join(" ")
+#   return reversed
+# end
+# why does map/collect work and each does not?  How do I do it with each?
 def reverse_each_word(string)
-  reversed = (string.split.collect do |word|
+  reverse_words = []
+  reverse_words << string.split.each do |word|
     word.reverse
-  end).join(" ")
-  return reversed
+  end.join(" ")
+  return reverse_words
 end
-# why does map work and each does not?  How do I do it with each?
-
 
